e221a2f7a0bb239cfbb3c734da22fb63912e162f
mingregister <774262573@qq.com>
2020-04-12 01:25:21 +0800

    go python 实现对比

--- 00简单工厂模式/simple-factory.go
+++ 00简单工厂模式/simple-factory.go
@@ -2,25 +2,20 @@ package main
 
 import "fmt"
 
-//
-//
+//from abc import ABCMeta, abstractclassmethod 
 
+// // 相当于抽象类
+// class API(metaclass=ABCMeta):
+//     @abstractclassmethod
+//     def Say(self, name):
+//         pass
 type API interface {
 	Say(name string) string
 }
 
-func NewAPI(str string) API {
-	if str == "en" {
-		return &English{}
-	} else if str == "cn" {
-		return &Chinese{}
-		// step 3
-	} else if str == "jp" {
-		return &Japanese{}
-	} else {
-		return nil
-	}
-}
+// class Chinese(API):
+//     def Say(self, name):
+//         print("你好, %s" % name)
 
 type Chinese struct{}
 
@@ -28,6 +23,7 @@ func (*Chinese) Say(name string) string {
 	return "你好" + " " + name
 }
 
+// class English(API)
 type English struct{}
 
 func (*English) Say(name string) string {
@@ -43,6 +39,35 @@ func (*Japanese) Say(name string) string {
 	return "日本人你好" + " " + name
 }
 
+// class NewAPI():
+//     def create_api(self, str):
+//         if str == "en":
+//             return English()
+//         elif str == "cn":
+//             return Chinese()
+//         elif str == "jp":
+//             return Japanese()
+//         else:
+//             return None
+
+func NewAPI(str string) API {
+	if str == "en" {
+		return &English{}
+	} else if str == "cn" {
+		return &Chinese{}
+		// step 3
+	} else if str == "jp" {
+		return &Japanese{}
+	} else {
+		return nil
+	}
+}
+
+// str1 = "en"
+// name = "Trump"
+// api = NewAPI()
+// lang = api.create_api(str1)
+// lang.Say(name)
 func main() {
 	str := "en"
 	name := "Trump"
