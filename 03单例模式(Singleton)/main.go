3fb0ef6a7c4fe55484223b9156dc941f7d4406b8
mingregister <774262573@qq.com>
2020-04-13 23:01:34 +0800

    go 导入本地包

--- 03单例模式(Singleton)/main.go
+++ 03单例模式(Singleton)/main.go
@@ -1,11 +1,11 @@
-package main 
-                        
-import ( 
-    "./singleton"
-) 
-                        
-func main() { 
-    mSingleton, nSingleton := singleton.NewSingleton("hello"), singleton.NewSingleton("hi") 
-    mSingleton.SaySomething() 
-    nSingleton.SaySomething() 
+package main
+
+import (
+	"factory/singleton" // 导入本地包的时候，前面的factory是包的名称，后面那个是package--通常与目录名称相同
+)
+
+func main() {
+	mSingleton, nSingleton := singleton.NewSingleton("hello"), singleton.NewSingleton("hi")
+	mSingleton.SaySomething()
+	nSingleton.SaySomething()
 }
