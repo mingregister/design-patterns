0ebcd427569824a8e6e19fcaba00db589cf3e2a0
mingregister <774262573@qq.com>
2020-04-15 23:37:50 +0800

    modified Makefile

--- 01工厂方法模式(Factory method)/factory-luffycity.go
+++ 01工厂方法模式(Factory method)/factory-luffycity.go
@@ -18,7 +18,6 @@ func (*WechatPay) Pay(money string) string {
 	return "微信余额支付元:" + " " + money
 }
 
-
 // 上面的实现与简单工厂模式一样。
 type PaymentFactory interface {
 	Create_payment() Payment
@@ -44,7 +43,7 @@ func main() {
 	// // pay := AlipayFactory()   // 不是这样调用的，会报如下错误，
 	//      ./factory-luffycity.go:42:22: missing argument to conversion to AlipayFactory: AlipayFactory())
 	//      理解如下： AlipayFactory是一个结构体，而不是函数，所以你不能直接调用：AlipayFactory()。
-	//                观察：func (ap *AlipayFactory) Create_payment() Payment{}的写法， 
+	//                观察：func (ap *AlipayFactory) Create_payment() Payment{}的写法，
 	//                 Create_payment()是属于 *AlipayFactory 这个指针的值的函数。
 	//                 所以：用它的反操作，&AlipayFactory{}
 	p := pay.Create_payment()
