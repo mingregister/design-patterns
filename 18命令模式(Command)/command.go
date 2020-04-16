463bcd48678c1dd9ea7c4cfc838edd58880b29eb
mingregister <774262573@qq.com>
2020-04-16 15:57:01 +0800

    command

---
+++ 18命令模式(Command)/command.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+)
+
+var s_cmd make([]Command,3,5)
+
+type Invoker struct {
+	cmds s_cmd
+}
+
+type Ireceiver interface{
+	action()
+}
+
+func(i *Invoker)Store_command(command Command){
+	// p.cmds(command)
+	append(command, p.cmds)
+}
+
+func(i *Invoker)Execute_commands(){
+	for cmd in cmd{
+		cmd.Execute()
+	}
+}
+
+type Command interface{
+	Execute()
+}
+
+type ConcreteCommand struct{
+	receiver Ireceiver
+}
+
+func(c *ConcreteCommand)Execute(){
+	c.receiver.action()
+}
+
+type Receiver struct{}
+
+func(r *Receiver)action(){
+	fmt.Println("haha")
+}
+
+func main(){
+
+	receiver := &Receiver{}
+
+	concrete_command := &ConcreteCommand{receiver}
+
+	i = &Invoker{s}
+	i.Store_command(concrete_command)
+	i.Execute_commands()
+
+
+}
