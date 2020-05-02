c7e75e5af3ad577d88d047b37ad43a2b9f684efc
mingregister <774262573@qq.com>
2020-05-03 01:40:11 +0800

    增加go interface与python类的对比，增强理解

--- 00简单工厂模式/simple-factory-luffycity.go
+++ 00简单工厂模式/simple-factory-luffycity.go
@@ -2,11 +2,20 @@ package main
 
 import "fmt"
 
+
+// 相当于@abstractclassmethod
 type Payment interface {
 	Pay(money string) string
 }
 
+// 相当于抽象类：class Alipay(metaclass=ABCMeta)
 type Alipay struct{}
+
+// class Payment(metaclass=ABCMeta):
+//     @abstractclassmethod
+//     def pay(self, money):
+//         pass
+
 
 func (*Alipay) Pay(money string) string {
 	return "支付宝余额支付元:" + " " + money
