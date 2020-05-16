2e9245ee87db75de858175d1a40aa6822bb11d49
mingregister <774262573@qq.com>
2020-05-16 16:13:58 +0800

    go impl guru.go

--- 13策略模式(strategy)/guru.go
+++ 13策略模式(strategy)/guru.go
@@ -3,43 +3,53 @@ package main
 // 尚未完成
 
 import (
-    "fmt"
+	"fmt"
 )
 
 type IStrategy interface {
-    DoAlgorithm()
+	DoAlgorithm()
 }
 
 type Context struct {
-    strategy IStrategy
+	strategy IStrategy
 }
 
 func (c *Context) DoSomeBusinessLogic() {
-    c.strategy.DoAlgorithm()
+	c.strategy.DoAlgorithm()
 }
 
 func initContext(s IStrategy) *Context {
-    return &Context{
-        strategy: s,
-    }
+	return &Context{
+		strategy: s,
+	}
 }
 
 // set strategy
 func (c *Context) SetStrategy(s IStrategy) {
-    // 我可以通过结构体中的参数来传递*类实例*的参数，相当于python中的self.strategy=s
-    c.strategy = s
+	// 我可以通过结构体中的参数来传递*类实例*的参数，相当于python中的self.strategy=s
+	c.strategy = s
 }
 
 type ConcreteStrategyA struct {
 }
 
 func (s *ConcreteStrategyA) DoAlgorithm() {
-    fmt.Println("ConcreteStrategyA")
+	fmt.Println("ConcreteStrategyA")
 }
 
-func main(){
-    csa := &ConcreteStrategyA{}
-    context := initContext(csa)
-    context.DoSomeBusinessLogic()
+type ConcreteStrategyB struct {
 }
 
+func (s *ConcreteStrategyB) DoAlgorithm() {
+	fmt.Println("ConcreteStrategyB")
+}
+
+func main() {
+	csa := &ConcreteStrategyA{}
+	context := initContext(csa)
+	context.DoSomeBusinessLogic()
+
+	csb := &ConcreteStrategyB{}
+	context.SetStrategy(csb)
+	context.DoSomeBusinessLogic()
+}
